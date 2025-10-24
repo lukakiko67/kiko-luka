31836fb2c7b60731e4da48618eac09e72e6ecdef
ivan <ivanbosevski86@gmail.com>
2025-10-24 13:20:19 +0200

    test

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,8 @@
+﻿
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Transform previousRoom;
+    [SerializeField] private Transform newRoom;
+} //3:14 6 epizoda
