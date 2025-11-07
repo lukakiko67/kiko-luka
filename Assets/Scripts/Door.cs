a6cdfba5ad3649aa37ad968342cceb908c768807
ivan <ivanbosevski86@gmail.com>
2025-11-07 12:57:11 +0100

    epizoda 6, 7, 8

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,8 +1,23 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     [SerializeField] private Transform previousRoom;
-    [SerializeField] private Transform newRoom;
-} //3:14 6 epizoda
+    [SerializeField] private Transform nextRoom;
+    [SerializeField] private CameraController cam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (collision.transform.position.x < transform.position.x)
+                cam.MoveToNewRoom(nextRoom);
+            else
+                cam.MoveToNewRoom(previousRoom);
+        }
+    }
+    private void Awake()
+    {
+        cam = Camera.main.GetComponent<CameraController>();
+    }
+}
