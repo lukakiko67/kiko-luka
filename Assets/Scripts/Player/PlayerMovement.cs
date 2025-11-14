2013bfd2be48166c09cf874ea1424d837dad464f
ivan <ivanbosevski86@gmail.com>
2025-11-14 12:49:52 +0100

    epizoda 9, malce 10
    .

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -22,14 +22,15 @@ public class PlayerMovement : MonoBehaviour
         private void Update()
      {
             horizontalInput = Input.GetAxis("Horizontal");
-          
+
 
         if (horizontalInput > 0.01f)
-            transform.localScale = Vector3.one;
-        else if(horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-1,1,1);
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
 
-       
+
+
 
         anim.SetBool("run",horizontalInput != 0);
         anim.SetBool("grounded", isGrounded());
